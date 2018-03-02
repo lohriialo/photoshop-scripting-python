40b820594d1dc0c8440d07e332b9ad139b4a67ba
Lohrii Alo <lohriigeorge@hotmail.com>
2018-03-02 19:38:04 +0530

    more code snippets

--- ActiveLayer.py
+++ ActiveLayer.py
@@ -3,8 +3,7 @@
 
 from win32com.client import Dispatch, GetActiveObject, GetObject
 # Start up Photoshop application
-# app = win32com.client.Dispatch('Photoshop.Application')
-
+# app = Dispatch('Photoshop.Application')
 # Or get Reference to already running Photoshop application instance
 # app = GetObject(Class="Photoshop.Application")
 app = GetActiveObject("Photoshop.Application")
